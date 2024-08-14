69cd50260f86a2a96f3197d5b4e5600bf45f2eee
geonwoo kwon <46738866+kay01-kwon@users.noreply.github.com>
2024-08-14 13:06:37 +0900

    Add files via upload

---
+++ gauss newton method/execute_gauss_newton.m
@@ -0,0 +1,40 @@
+close all
+clear all
+clc
+
+
+param_true = [10;10];
+
+x = 0:0.01:10;
+x = x';
+N = length(x);
+y = zeros(N,1);
+
+for i = 1:N
+    y(i) = actual_func(x(i), param_true) + normrnd(0,0.2);
+end
+meas = [x, y];
+
+param0 = [0.5;0.5];
+
+param_est = gauss_newton(@(x,p) model_func(x,p), param0, meas, 1e-4, 30);
+
+y_model = zeros(N,1);
+
+for i = 1:N
+    y_model(i) = model_func(x(i), param_est);
+end
+
+
+figure()
+
+plot(x, y);
+hold on;
+plot(x, y_model)
+grid on
+
+
+
+function y = actual_func(x, param)
+y = param(1)*x/(param(2)+x);
+end
---
+++ gauss newton method/gauss_newton.m
@@ -0,0 +1,62 @@
+function param_k = gauss_newton(model_func, param0, meas, tol, max_iter)
+%GAUSS_NEWTON 이 함수의 요약 설명 위치
+%   [Function value, Gradient] = model_func(x)
+%   meas(i,j) --> i: measurement step, 
+%   j = 1: input
+%   j = 2: output
+
+[m, ~] = size(meas);
+[n, ~] = size(param0);
+
+residual = zeros(m,1);
+dresidual = zeros(n,1);
+
+y_model = zeros(m,1);
+
+
+figure()
+hold on
+
+param_k = param0;
+
+for i = 1:max_iter
+
+    rmse = 0;
+
+    A = zeros(n,n);
+    b = zeros(n,1);
+
+    for j = 1:m
+        [f, dresidual] = model_func(meas(j,1), param_k);
+        y_model(j) = f;
+        residual(j) = f - meas(j,2);
+        b = b + residual(j)*dresidual;
+        A = A + dresidual * dresidual';
+        rmse = rmse + residual(j)^2;
+    end
+
+
+    p = -inv(A)*b;
+
+    param_k = param_k + p;
+    
+    clf
+    subplot(121)
+    histogram(residual)
+
+    subplot(122)
+    plot(meas(:,1), meas(:,2));
+    hold on
+    plot(meas(:,1), y_model);
+
+    grid on
+
+    pause(0.5)
+
+    exportgraphics(gcf, 'test_gauss_newton.gif','Resolution',600,'Append',true);
+    
+
+    if rmse < tol
+        break;
+    end
+end
