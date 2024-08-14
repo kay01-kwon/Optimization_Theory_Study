792eef130fae7b6ef2d9561db6977d1f42a79c93
geonwoo kwon <46738866+kay01-kwon@users.noreply.github.com>
2024-08-14 16:13:09 +0900

    Add files via upload

---
+++ gauss newton method/model_func.m
@@ -0,0 +1,14 @@
+function [f,g] = model_func(x, param)
+%MODEL_FUNC 이 함수의 요약 설명 위치
+%   자세한 설명 위치
+
+f = param(1)*x/(param(2)+x);
+
+g_p1 = x/(param(2)+x);
+
+g_p2 = -param(1)/(param(2)+x)^2;
+
+g = [g_p1;g_p2];
+
+end
+
